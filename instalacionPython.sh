9f5c0a3dfc400e75271ee20f57c297a865179f6b
FEB-col <fabianbohor@hotmail.com>
2017-05-14 03:24:50 -0500

    Archivo de Configuracion 2

--- instalacionPython.sh
+++ instalacionPython.sh
@@ -1,8 +1,15 @@
+#this file install python
+#install debian or ubuntu
+#execute Super Admin
 su
+#install update for all SO
 apt-get update
 apt-get upgrade
+#install python version 3.5.4 the max version
 apt-get install python3
+#install asisten
 apt-get install python3-pip
+#install librarys
 apt-get install python3-requests
 apt-get install python3-bs4
 
